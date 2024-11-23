cc152953e9721d38583bb8128eaf27e3653ed00c
Daniel Benalcazar <danielbenalcazar87@gmail.com>
2024-11-23 08:14:24 -0500

    Disabled tensor normalization

--- onnx_convert.py
+++ onnx_convert.py
@@ -1,7 +1,7 @@
 import os
 import torch
 from PIL import Image
-from torchvision import transforms as T
+from torchvision.transforms import v2
 from models.classification import load_model
 from models.siamese import siamese_embeddings
 from argparse import ArgumentParser as argparse
@@ -47,7 +47,11 @@ print('{}_{} successfully loaded in {}'.format(args.backbone, args.model_type, d
 print('Number of parameters: {:d}\n'.format(n_params))
 
 # Generate Dummy input
-tform = T.Compose([T.Resize((H,W)), T.ToTensor()])
+tform = v2.Compose([
+            v2.PILToTensor(),
+            #v2.ToDtype(torch.float32, scale=True),
+            #v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
 dummy_input = Image.new(MODE, RESOLUTION, (128, 128, 128))
 dummy_input = tform(dummy_input).unsqueeze(0)
 
